8ce45f1a9f555e12f81db3c4e4db2a1734c38988
fcecilie <flavian.cecilien@laposte.net>
2021-03-01 02:11:11 +0100

    Init bundle

---
+++ src/DependencyInjection/ArcadiaValidatorExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Arcadia\Bundle\ValidatorBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ArcadiaValidatorExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
---
+++ src/Service/ValidatorService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Arcadia\Bundle\ValidatorBundle;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ValidatorService
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function getFailedValidationResponse($data, $constraints = null, $groups = null): ?Response
+    {
+        $violations = $this->validator->validate($data, $constraints, $groups);
+
+        if ($violations->count() <= 0) {
+            return null;
+        }
+
+        $messages = [];
+
+        /** @var ConstraintViolationInterface $violation */
+        foreach ($violations as $violation) {
+            $property = $violation->getPropertyPath();
+            $messages[$property][] = $violation->getMessage();
+        }
+
+        $responseData = [
+            'status' => Response::HTTP_BAD_REQUEST,
+            'message' => "Validation failed",
+            'errors' => $messages,
+        ];
+
+        return new JsonResponse($responseData, Response::HTTP_BAD_REQUEST);
+    }
+}
